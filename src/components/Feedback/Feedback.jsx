e21ce0bfe6ca5c3b643b6f662d38bffcd7413f43
NataliiaSav <savchenko6561@gmail.com>
2023-06-05 19:52:56 +0300

    fix feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,25 +1,20 @@
-import React, { Component } from "react";
+import React  from "react";
 import PropTypes from "prop-types";
 import css from './Feedback.module.css'
 
-export class Feedback extends Component {
-    render() {
-        const { options } = this.props;
-
+export const Feedback = ({options, onLeaveFeedback}) => {
         return (
             <>
             <div className = {css.buttons}>
                     {options.map(option =>{
                         return (
-                            <button className = {css.btn} key = {option} type = 'button' onClick={() => this.props.onLeaveFeedback(option)}>{option}</button>
+                            <button className = {css.btn} key = {option} type = 'button' onClick={() => onLeaveFeedback(option)}>{option}</button>
                         )
                     })}
                 </div>
                 </>
         )
-    }
 }
-
 Feedback.propTypes = {
     options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
     onLeaveFeedback: PropTypes.func.isRequired,
